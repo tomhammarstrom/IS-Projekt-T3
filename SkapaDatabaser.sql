d14196705cb74bb047b1b5bb686580b613c3bad7
Johannes Sörensen <jsorensen.m@gmail.com>
2013-12-07 09:36:16 +0100

    ny tabell enl ny instruktion

--- SkapaDatabaser.sql
+++ SkapaDatabaser.sql
@@ -1,7 +1,11 @@
 create table student(civic varchar(20) not null, name varchar(20), constraint s_pk PRIMARY KEY(civic))
 create table course(courseId varchar(20) not null, name varchar(20), constraint c_pk PRIMARY KEY(courseId))
 
-create table studies(civic varchar(20) not null, courseId varchar(20) not null, isActive bit not null, grade varchar(5), 
+create table studies_active(civic varchar(20) not null, courseId varchar(20) not null, grade varchar(5), 
+constraint st_pk PRIMARY KEY(civic,courseId), constraint st_fk1 FOREIGN KEY(civic) references student(civic),
+constraint st_fk2 FOREIGN KEY(courseId) references course(courseId))
+
+create table studies_inactive(civic varchar(20) not null, courseId varchar(20) not null, grade varchar(5), 
 constraint st_pk PRIMARY KEY(civic,courseId), constraint st_fk1 FOREIGN KEY(civic) references student(civic),
 constraint st_fk2 FOREIGN KEY(courseId) references course(courseId))
 
@@ -13,5 +17,5 @@ insert into course values('KursKodA01', 'KursNameAAA')
 insert into course values('KursKodB02', 'KursNameBBB')
 insert into course values('KursKodC03', 'KursNameCCC')
 
-insert into studies values('111', 'KursKodA01', '0','A')
-insert into studies values('222', 'KursKodA01', '1', null)
+insert into studies_active values('111', 'KursKodA01','A')
+insert into studies_active values('222', 'KursKodA01', null)
