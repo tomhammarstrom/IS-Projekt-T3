58603b8b1c9d4814f3d789df7104e5c8c521166b
Johannes Sörensen <jsorensen.m@gmail.com>
2013-12-07 10:00:07 +0100

    student

--- isprojekt/src/model/Student.java
+++ isprojekt/src/model/Student.java
@@ -5,7 +5,24 @@ public class Student {
     private String name;
 
 
-    public Student() {
-        super();
+    public Student(String civic) {
+        setCivic(civic);
+    }
+    
+    
+    public void setCivic(String civic){
+        this.civic = civic;
+    }
+    
+    public String getCivic(){
+        return civic;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
     }
 }
