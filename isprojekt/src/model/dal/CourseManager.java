1bcb831bc9f91011483052459f0b6c8eea6da46e
Johannes Sörensen <jsorensen.m@gmail.com>
2013-12-07 15:00:02 +0100

    tror vi delar upp dessa i två

---
+++ isprojekt/src/model/dal/CourseManager.java
@@ -0,0 +1,12 @@
+package isprojekt.src.model.dal;
+
+import isprojekt.src.controller.Controller;
+
+public class CourseManager {
+    private Controller con;
+    
+    public CourseManager(Controller con){
+        this.con = con;
+    }
+
+}
