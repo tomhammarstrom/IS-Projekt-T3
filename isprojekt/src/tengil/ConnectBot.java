e23f52bfde9f88ebe9b957ece7a15c997f8533f9
Johannes Sörensen <jsorensen.m@gmail.com>
2013-12-09 13:26:08 +0100

    jdocs och avslutad connect bot

--- isprojekt/src/tengil/ConnectBot.java
+++ isprojekt/src/tengil/ConnectBot.java
@@ -10,26 +10,44 @@ import java.sql.Statement;
 public class ConnectBot {
     private Connection connection1;
 
-
+    /**
+     * Ansluter till databasen, körs vid uppstart
+     * @throws SQLException
+     */
     public void connect() throws SQLException {
         DriverManager.registerDriver(new sun.jdbc.odbc.JdbcOdbcDriver());
         connection1 = DriverManager.getConnection("JDBC:ODBC:isprojekt_new");
     }
     
+    /**
+     * Bygger preparedstatments med connection1
+     * @param request
+     * @return
+     * @throws SQLException
+     */
     public PreparedStatement buildStatement(String request) throws SQLException {
         System.out.println(request);
         return connection1.prepareStatement(request);
     }
 
-
+    /**
+     * Läs-operationer
+     * @param request
+     * @return
+     * @throws SQLException
+     */
     public ResultSet query(PreparedStatement request) throws SQLException {
         return request.executeQuery();
-
     }
     
+    /**
+     * Skriv-operationer
+     * @param request
+     * @return
+     * @throws SQLException
+     */
     public int update(PreparedStatement request) throws SQLException {
-        return request.executeUpdate();
-                                                      
+        return request.executeUpdate();                                       
     }
 
 }
