ac681e9deb77f235d7dcd21d169e80eec9fedcd4
Johannes Sörensen <jsorensen.m@gmail.com>
2013-12-10 20:20:58 +0100

    reset

--- isprojekt/src/view/MainFrame.java
+++ isprojekt/src/view/MainFrame.java
@@ -1,22 +1,29 @@
 package view;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTabbedPane;
 
 import tengil.Controller;
+
 import javax.swing.JList;
 
+@SuppressWarnings("serial")
 public class MainFrame extends JFrame {
 	private Controller con;
 	
 
-	private JPanel contentPane;
-	private JPanel panel;
-	private JPanel panel_1;
-	private JList list;
-	private JList list_1;
+	private JPanel contentPane = new JPanel();
+	private JPanel panel = new JPanel();
+	private JPanel panel_1 = new JPanel();
+	
+	private DefaultListModel studentListModel = new DefaultListModel();
+	
+	private JList list = new JList(studentListModel);
+	private JList list_1 = new JList();
+	
 
 	public MainFrame(Controller con) {
 		this.con = con;
@@ -26,7 +33,7 @@ public class MainFrame extends JFrame {
 	private void initComponents(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1241, 589);
-		contentPane = new JPanel();
+		
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -35,20 +42,25 @@ public class MainFrame extends JFrame {
 		tabbedPane.setBounds(12, 13, 343, 516);
 		contentPane.add(tabbedPane);
 		
-		panel = new JPanel();
+		
 		tabbedPane.addTab("New tab", null, panel, null);
 		panel.setLayout(null);
 		
-		list = new JList();
+		
 		list.setBounds(12, 13, 314, 419);
 		panel.add(list);
 		
-		panel_1 = new JPanel();
+		
 		tabbedPane.addTab("New tab", null, panel_1, null);
 		panel_1.setLayout(null);
 		
-		list_1 = new JList();
+		
 		list_1.setBounds(12, 13, 314, 421);
 		panel_1.add(list_1);
+		
+	
 	}
+
+
+
 }
