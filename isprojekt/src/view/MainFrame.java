3cacb6402cdc006454cb92ad641c97f98833761a
Johannes Sörensen <jsorensen.m@gmail.com>
2013-12-12 10:19:21 +0100

    julstädning

--- isprojekt/src/view/MainFrame.java
+++ isprojekt/src/view/MainFrame.java
@@ -7,12 +7,9 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.JTabbedPane;
-
 import tengil.Controller;
-
 import javax.swing.JList;
 import javax.swing.JButton;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.sql.ResultSet;
@@ -22,25 +19,22 @@ import java.sql.SQLException;
 public class MainFrame extends JFrame {
 	private Controller con;
 	
-
+	private JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 	private JPanel contentPane = new JPanel();
 	private JPanel studentTabPanel = new JPanel();
 	private JPanel courseTabPanel = new JPanel(); 
+	private JPanel studentPanel;
 	
 	private DefaultListModel<String> studentListModel = new DefaultListModel<String>();
-	private JList<String> studentList = new JList<String>(studentListModel);
-	
 	private DefaultListModel<String> courseListModel = new DefaultListModel<String>();
+	
+	private JList<String> studentList = new JList<String>(studentListModel);
 	private JList<String> courseList = new JList<String>(courseListModel);
 	
+	private JButton addStudentButton = new JButton("LEG TILL ELEF");
+	private JButton addCourseButton = new JButton("LEG TILL KURZ");
 	
-	
-	
-	
-	private JButton btnLggTillStudent = new JButton("LEG TILL ELEF");
-	private JButton btnLggTillKurs = new JButton("LEG TILL KURZ");
-	
-
+	//konstruktor
 	public MainFrame(Controller con) {
 		this.con = con;
 		initComponents();
@@ -53,10 +47,11 @@ public class MainFrame extends JFrame {
 		}
 	}
 	
-	
+	// hämtar data och placerar den i listorna över studenter/kurser
 	public void populateList() throws SQLException{
 		studentListModel.clear();
 		courseListModel.clear();
+		
 		ResultSet allStudents = con.getStudents();
 		ResultSet allCourses = con.getCourses();
 		
@@ -70,63 +65,60 @@ public class MainFrame extends JFrame {
 		
 	}
 	
+	//skapar alla grafiska komponenter
 	private void initComponents(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1241, 589);
-		
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		
-		contentPane.setLayout(null);
-		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		studentList.setBounds(12, 13, 314, 419);
+		courseList.setBounds(12, 13, 314, 421);
+		addStudentButton.setBounds(87, 448, 152, 25);
 		tabbedPane.setBounds(12, 13, 343, 516);
+		addCourseButton.setBounds(98, 448, 124, 25);
+		
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
 		contentPane.add(tabbedPane);
-		
-		
+
 		tabbedPane.addTab("Studenter", null, studentTabPanel, null);
 		tabbedPane.addTab("Kurser", null, courseTabPanel, null);
 		
 		studentTabPanel.setLayout(null);
-		courseTabPanel.setLayout(null);
-		
-		studentList.setBounds(12, 13, 314, 419);
-		courseList.setBounds(12, 13, 314, 421);
-		
 		studentTabPanel.add(studentList);
-		btnLggTillStudent.addActionListener(new ActionListener() {
+		studentTabPanel.add(addStudentButton); 
+		
+		courseTabPanel.setLayout(null);
+		courseTabPanel.add(courseList);
+		courseTabPanel.add(addCourseButton);
+		
+		addStudentButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				addStudent();
 			}
 		});
-		btnLggTillStudent.setBounds(87, 448, 152, 25);
 		
-		studentTabPanel.add(btnLggTillStudent); 
-		courseTabPanel.add(courseList);
-		btnLggTillKurs.addActionListener(new ActionListener() {
+		addCourseButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				addCourse();
 			}
 		});
-		btnLggTillKurs.setBounds(98, 448, 124, 25);
-		
-		courseTabPanel.add(btnLggTillKurs);
 		
 		studentList.addListSelectionListener(new ListSelectionListener() {
 	            public void valueChanged(ListSelectionEvent e) {
 	                studentList_valueChanged(e);
 	                    }
 	                });
+		
 		courseList.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent e) {
 				courseList_valueChanged(e);
 			}
 		});
-
 		
 	}
 	
+	//listener för studentlistan
 	private void studentList_valueChanged(ListSelectionEvent e){
 		 	try{
 		 		openStudent(studentList.getSelectedValue().toString());
@@ -134,22 +126,26 @@ public class MainFrame extends JFrame {
 		 	catch(NullPointerException f){
 		 	}
 	    }
+	
+	//listener för kurslistan
 	private void courseList_valueChanged(ListSelectionEvent e){
 		try{
 			openCourse(courseList.getSelectedValue().toString());
 		}
 		catch(NullPointerException f){
 		}
-	}
+	 }
 	 
-	 private void addStudent(){
+	
+	//delegerare för att skapa nya studenter/kurser
+	private void addStudent(){
 		 openStudent(null);
 	 }
-	 private void addCourse(){
+	private void addCourse(){
 		 openCourse(null);
 	 }
 
-	 private JPanel studentPanel;
+	 //öppna en existerande student eller skapa en ny
 	 private void openStudent(String civic){
 		 try{
 				contentPane.remove(studentPanel);
@@ -161,6 +157,8 @@ public class MainFrame extends JFrame {
 		repaint();
 		contentPane.add(studentPanel);
 	} 
+	 
+	 //öppna eller skapa en kurs
 	 private void openCourse(String id){
 		 try{
 				contentPane.remove(studentPanel);
@@ -173,6 +171,7 @@ public class MainFrame extends JFrame {
 		contentPane.add(studentPanel);
 	}
 	 
+	 //töm kurs-/studentpanelen, används innan en ny genereras
 	 public void clearPanel(){
 		 remove(studentPanel);
 		 repaint();
