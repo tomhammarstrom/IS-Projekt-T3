fc1cc43ef20379382a10f6f45ce7aa15486ce39c
Johannes Sörensen <jsorensen.m@gmail.com>
2013-12-12 10:22:09 +0100

    påskstädning

--- isprojekt/src/view/NewCoursePanel.java
+++ isprojekt/src/view/NewCoursePanel.java
@@ -19,10 +19,12 @@ public class NewCoursePanel extends JPanel {
 	private Controller con;
 	private String currentCourse; 
 	private MainFrame mainFrame;
+	
 	private JTextField pointsField = new JTextField();
 	private JTextField idField = new JTextField();
 	private JTextField nameField = new JTextField();
 	private JTextField descrField = new JTextField();
+	
 	private JLabel idLbl = new JLabel("Kurs ID");
 	private JLabel nameLbl = new JLabel("Namn");
 	private JLabel pointsLbl = new JLabel("Poäng");
@@ -30,9 +32,7 @@ public class NewCoursePanel extends JPanel {
 	private JButton saveBtn = new JButton("Spara");
 	private JButton deleteBtn = new JButton("Ta bort");
 
-	/**
-	 * Create the panel.
-	 */
+	// konstruktor
 	public NewCoursePanel(Controller con, String id, MainFrame mainFrame) {
 		this.con = con;
 		currentCourse = id;
@@ -46,10 +46,10 @@ public class NewCoursePanel extends JPanel {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
-		
+	
 	}
 	
+	// hämtar data för öppnade kurser
 	private void existingData() throws SQLException{
 		if(currentCourse != null){	
 			ResultSet r = con.getCourse(currentCourse);
@@ -66,7 +66,12 @@ public class NewCoursePanel extends JPanel {
 		}
 		 
 	}
+	
+	//skapar alla grafiska komponenter
 	private void initComponents(){
+		setBounds(397, 13, 810, 516);
+		setLayout(null);
+		
 		descrField.setBounds(179, 201, 140, 20);
 		descrField.setColumns(10);
 		nameField.setBounds(179, 82, 140, 20);
@@ -75,37 +80,38 @@ public class NewCoursePanel extends JPanel {
 		idField.setColumns(10);
 		pointsField.setBounds(179, 140, 140, 20);
 		pointsField.setColumns(10);
-		setBounds(397, 13, 810, 516);
-		setLayout(null);
+		idLbl.setBounds(49, 31, 46, 14);
+		nameLbl.setBounds(49, 85, 46, 14);
+		descrLbl.setBounds(49, 204, 76, 14);
+		pointsLbl.setBounds(49, 143, 46, 14);
+		saveBtn.setBounds(288, 480, 89, 23);
+		deleteBtn.setBounds(12, 479, 97, 25);
 		
 		add(pointsField);		
 		add(idField);		
 		add(nameField);		
 		add(descrField);
-		idLbl.setBounds(49, 31, 46, 14);		
-		add(idLbl);
-		nameLbl.setBounds(49, 85, 46, 14);		
-		add(nameLbl);
-		pointsLbl.setBounds(49, 143, 46, 14);		
-		add(pointsLbl);
-		descrLbl.setBounds(49, 204, 76, 14);		
+		add(idLbl);	
+		add(nameLbl);	
+		add(pointsLbl);	
 		add(descrLbl);
+		add(saveBtn);
+		
 		saveBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				addCourse();
 			}
 		});
-		saveBtn.setBounds(288, 480, 89, 23);		
-		add(saveBtn);
+				
 		deleteBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				deleteCourse();
 			}
 		});
-		deleteBtn.setBounds(12, 479, 97, 25);
-		
 		
 	}
+	
+	//körs att addCourse() för att se till så att det finns poäng(int) och ett id
 	private boolean validateInput(){
 		if (con.validateNotNull(idField.getText()) && con.validateNotNull(pointsField.getText()) && con.validateNumbers(pointsField.getText())){
 			return true;
@@ -114,6 +120,8 @@ public class NewCoursePanel extends JPanel {
 			return false;
 		}
 	}
+	
+	//lägger till eller ändrar en kurs
 	private void addCourse(){
 		if(validateInput()){
 			int success = 0;
@@ -147,9 +155,9 @@ public class NewCoursePanel extends JPanel {
 			JOptionPane.showMessageDialog(null, "Var god fyll i ett id och ett poängantal (i siffror)");
 		}
 		
-		
 	}
 	
+	//tar bort en kurs
 	private void deleteCourse(){
 		try {
 			con.removeCourse(currentCourse);
