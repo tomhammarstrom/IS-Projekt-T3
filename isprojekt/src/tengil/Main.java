2925d52c165421eb75a4d2baff8a9d2e6b5baec9
matildasvensson <sys12msv@student.lu.se>
2013-12-12 12:43:56 +0100

    wazzzupp

--- isprojekt/src/tengil/Main.java
+++ isprojekt/src/tengil/Main.java
@@ -11,6 +11,8 @@ public class Main {
 
     public static void main (String []args) throws SQLException {
     	new MainFrame(new Controller()).setVisible(true);
+    	
+    	//matilda was here
 
     }
     
