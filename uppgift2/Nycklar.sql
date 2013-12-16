722786acbc1f309e992aca67d6576d1e13567994
Johannes Sörensen <jsorensen.m@gmail.com>
2013-12-16 10:33:43 +0100

    primary key

--- uppgift2/Nycklar.sql
+++ uppgift2/Nycklar.sql
@@ -1,2 +1,7 @@
 EXEC sp_pkeys [CRONUS Sverige AB$Employee]
 EXEC sp_helpconstraint [CRONUS Sverige AB$Employee]
+
+
+SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+where TABLE_NAME = 'CRONUS Sverige AB$Employee'
+
