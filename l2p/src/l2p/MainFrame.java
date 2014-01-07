4dab5f34f006f684754a5be6382a37b8ca0cf975
Johannes Sörensen <jsorensen.m@gmail.com>
2014-01-07 18:11:27 +0100

    resten

--- l2p/src/l2p/MainFrame.java
+++ l2p/src/l2p/MainFrame.java
@@ -4,10 +4,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+
 import java.awt.List;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import java.sql.*;
+
+@SuppressWarnings("serial")
 public class MainFrame extends JFrame {
 
 	/**
@@ -54,13 +58,43 @@ public class MainFrame extends JFrame {
 		// listener för knappiz
 		knappiz.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				testMetod();
-				// testMetod, körs när man trycker på knappen, se nedan
+				try {
+					testMetod();
+					// testMetod, körs när man trycker på knappen, se nedan
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				
 			}
 		});
 	}
 	
-	private void testMetod(){
+	
+	/**
+	 * Metod för att hämta namnen på alla studenter, körs när man trycker på knappen
+	 */
+	private void testMetod() throws SQLException{
+		// Att "prata" med SQL görs i 4 steg, steg 1 (vilket bara behöver göras 1 gång) är att registrera en drivrutin
+		DriverManager.registerDriver(new sun.jdbc.odbc.JdbcOdbcDriver());
+		
+		// Steg 2 är att skapa en anslutning, viktigt här är den sista delen (isprojekt_cool) som ska vara namnet på din databas
+		// Innan den kan användas, måste den registreras i controlpanelen på datorn (se powerpoint)
+		Connection connection = DriverManager.getConnection("JDBC:ODBC:isprojekt_cool");
+		
+		// Steg 3 är att skapa ett statement
+		Statement statement = connection.createStatement();
+		
+		// Steg 4 är att "skicka" en SQL-sats till databasen (se parantesen), denna sparas sen i ett s.k. "result set"
+		// Detta inehåller samma tabel-info du ser i SQL-server management
+		ResultSet r = statement.executeQuery("select * from student");
+		
+		//För att sedan "redovisa" vad vi hämtade, väljer vi att lägga alla namn i listan.
+		
+		while(r.next()){ //så länge r har fler rader
+			listan.add(r.getString(2)); // 2 - eftersom namnet har kolumnposition 2 i SQL
+		}
+		
+		
 		
 	}
 }
