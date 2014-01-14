9917932cd9414836851c4cef1dfd188446af5bb2
Johannes Sörensen <jsorensen.m@gmail.com>
2014-01-14 23:42:57 +0100

    allt utom paths

--- uppgift3/src/uppgift3/FileManager.java
+++ uppgift3/src/uppgift3/FileManager.java
@@ -4,40 +4,108 @@ import java.io.IOException;
 
 public class FileManager {
 	
+	private void openFile(String[] cmdarray){
+		try {
+			Runtime.getRuntime().exec(cmdarray);
+		} catch (IOException e) {
+			System.out.println("File not found");
+		}
+	}
+	
+	/*
+	 * Excel
+	 */
+	public void openExcel1(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openExcel2(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openExcel3(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openExcel4(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openExcel5(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openExcel6(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openExcel7(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+
+	
+	/*
+	 * Access
+	 */
+	public void openAccess1(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openAccess2(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openAccess3(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openAccess4(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openAccess5(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openAccess6(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openAccess7(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+
+	
+	/*
+	 * SQL
+	 */
+	public void openSql1(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openSql2(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openSql3(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openSql4(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openSql5(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openSql6(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	public void openSql7(){
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
+	}
+	
+	
+	/*
+	 * Forms
+	 */
 	public void openForm1(){
-		try {
-			String[] cmdarray = new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\Rapport_customer.docx" };
-			Runtime.getRuntime().exec(cmdarray);
-		} catch (IOException e) {
-			System.out.println("File not found");
-		}
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
 	}
-	
 	public void openForm2(){
-		try {
-			String[] cmdarray = new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\Rapport_customer.docx" };
-			Runtime.getRuntime().exec(cmdarray);
-		} catch (IOException e) {
-			System.out.println("File not found");
-		}
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
 	}
-	
 	public void openForm3(){
-		try {
-			String[] cmdarray = new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\Rapport_customer.docx" };
-			Runtime.getRuntime().exec(cmdarray);
-		} catch (IOException e) {
-			System.out.println("File not found");
-		}
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
 	}
-	
 	public void openForm4(){
-		try {
-			String[] cmdarray = new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\Rapport_customer.docx" };
-			Runtime.getRuntime().exec(cmdarray);
-		} catch (IOException e) {
-			System.out.println("File not found");
-		}
+		openFile(new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\XXX" });
 	}
+
+	
 
 }
