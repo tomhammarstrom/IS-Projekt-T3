d91d3a03fea6e970aceb57eee98082604c680d5d
Johannes Sörensen <jsorensen.m@gmail.com>
2014-01-14 23:15:08 +0100

    hööörs

--- uppgift3/src/uppgift3/FileManager.java
+++ uppgift3/src/uppgift3/FileManager.java
@@ -4,14 +4,37 @@ import java.io.IOException;
 
 public class FileManager {
 	
-	public void openExcel1(){
+	public void openForm1(){
 		try {
-			// Runtime.getRuntime().exec("\"D:\\prog file\\090422.xls\"");
 			String[] cmdarray = new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\Rapport_customer.docx" };
 			Runtime.getRuntime().exec(cmdarray);
-			// or
-			// Runtime.getRuntime().exec("cmd /c start \"\" \"D:\\prog file\\090422.xls\"");
-
+		} catch (IOException e) {
+			System.out.println("File not found");
+		}
+	}
+	
+	public void openForm2(){
+		try {
+			String[] cmdarray = new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\Rapport_customer.docx" };
+			Runtime.getRuntime().exec(cmdarray);
+		} catch (IOException e) {
+			System.out.println("File not found");
+		}
+	}
+	
+	public void openForm3(){
+		try {
+			String[] cmdarray = new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\Rapport_customer.docx" };
+			Runtime.getRuntime().exec(cmdarray);
+		} catch (IOException e) {
+			System.out.println("File not found");
+		}
+	}
+	
+	public void openForm4(){
+		try {
+			String[] cmdarray = new String[] { "cmd.exe", "/c", "C:\\Users\\submarines\\git\\isprojekt\\uppgift3\\Filer\\Rapport_customer.docx" };
+			Runtime.getRuntime().exec(cmdarray);
 		} catch (IOException e) {
 			System.out.println("File not found");
 		}
