f09e42967a24aa7b3be5dff30244e96f2a337722
Johannes Sörensen <jsorensen.m@gmail.com>
2014-01-14 16:33:44 +0100

    shiet

--- uppgift2/src/uppgift2/Main.java
+++ uppgift2/src/uppgift2/Main.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class Main {
 	public static void main (String [] args) throws SQLException{
-		new MeinFrame().setVisible(true);
+		new MainFrame().setVisible(true);
 	}
 
 }
